d2850a1b9bc471e64bae5fda48d9680a90c1347e
Bagaspamungkas2720 <142129512+Bagaspamungkas2720@users.noreply.github.com>
2023-09-16 11:31:53 +0700

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int nomor = 1;
+    string nama = "superman"
+
+    cout<<nomor<<endl;
+    cout<<nama<<enddl;
+
+}
---
+++ Untitled2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string aktivitas;
+    string tempat;
+
+    cout<<"kamu kangen aku : ";
+    cin>>aktivitas;
+
+    cout<<"dimana "<<tempat<<" : ";
+    cin>>tempat;
+
+    cout<<"saya "<<aktivitas<<" kamu di "<<tempat<<endl;
+    cout<<"aku juga kangen kamu"<<endl;
+
+}
